8901401708ceafd69021da736a6aea3969dbfa88
Sam Cutler <sam.cutler@gmail.com>
2020-12-24 15:11:51 -0800

    The /pokemon endpoint is mostly functional

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use( (req, res) => {
-    res.send('Hello, ya filthy animal!');
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware running')
+    
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+    
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json( {error: 'Unauthorized request'} )
+    }
+
+    // move to next middleware
+    next()
+})
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+app.get('/types', function handleGetTypes(req, res) {
+    res.json(validTypes);
 });
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+
+    let response = POKEDEX.pokemon;
+
+    // filter by name if name query param used
+    if (req.query.name) {
+        response = response.filter(pokemon => 
+            pokemon.name.toLocaleLowerCase().includes(req.query.name.toLocaleLowerCase())
+        );
+    }
+
+    // filter pokemon by type if type query param present:
+    if (req.query.type) {
+        response = response.filter(pokemon => 
+            pokemon.type.includes(req.query.type)
+        );
+    }
+
+    res.send(response);
+})
+
 
 const PORT = 8000;
 
