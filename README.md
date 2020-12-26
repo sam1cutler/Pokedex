32c0bded6c3f56a2d1a0ff55a39bbc1ffcb1db6d
Sam Cutler <sam.cutler@gmail.com>
2020-12-26 11:38:03 -0800

    Prepared project for deploying to Heroku

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pokedex Server
+
+## General Description
+This server is intended as a tool to learn Node.js, Express, implementing CORS for an Express project, adding security with Helmet, and deploying a production version of the project to Heroku.
+
+## Technology used
+This project uses Node.js, Express, Morgan, and Nodemon. 
+
+## Contact me
+You can find [my GitHub page here](https://github.com/sam1cutler).
+
+## Acknowledgements
+This project is part of an assignment for the Thinkful software engineering program. 
--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-    console.log('validate bearer token middleware running')
     
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
@@ -52,9 +53,16 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
     res.send(response);
 })
 
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+})
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-});
+app.listen(PORT);
